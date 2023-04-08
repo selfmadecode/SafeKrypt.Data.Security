0fd1ed96926f223950f0223ba27c5e0f9a348231
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-04-08 21:57:00 +0100

    Restructure encrypt class

---
+++ Encrypt/AesEncryption/Encrypt.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace SafeCrypt
+{
+    public class Encrypt : BaseAesEncryption
+    {
+        public byte[] AesEncrypt(byte[] data, byte[] key, byte[] iv)
+        {
+            return EncryptAES(data, key, iv);
+        }
+
+        public string AesEncryptByteToHex(byte[] data, byte[] key, byte[] iv)
+        {
+            var cipherText = EncryptAES(data, key, iv);
+
+            return cipherText.ByteArrayToHexString();
+        }
+
+        public string AesEncryptByteToBase64(byte[] data, byte[] key, byte[] iv)
+        {
+            var cipherText = EncryptAES(data, key, iv);
+
+            return cipherText.ByteArrayToHexString();
+        }
+
+        //public byte[] AesEncrypt(byte[] data, byte[] key, byte[] iv, ReturnType returnType)
+        //{
+
+        //}        
+    }
+}
