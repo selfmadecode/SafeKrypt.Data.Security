ea99cd0aebd764b020775b859adf3a62a6ff42f3
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-04-08 08:59:04 +0100

    All methods should use the namespace Using SafeCrypt.Encrypt

---
+++ Encrypt/Encrypt.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace SafeCrypt
+{
+    public class Encrypt
+    {
+        // Method to encrypt data using AES algorithm
+        public static byte[] EncryptAES(byte[] data, byte[] key, byte[] iv)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(data, 0, data.Length);
+                        cryptoStream.FlushFinalBlock();
+                        return memoryStream.ToArray();
+                    }
+                }
+            }
+        }
+    }
+}
