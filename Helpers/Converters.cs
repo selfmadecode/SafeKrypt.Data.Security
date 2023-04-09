7a81b0746e9c1c2cf4a4f587a51975c2c90527d3
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-04-09 09:47:49 +0100

    Add Converter class that includes string converters extension methods

---
+++ Helpers/Converters.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SafeCrypt
+{
+    public static class Converters
+    {
+        public static string ByteArrayToHexString(this byte[] bytes)
+        {
+            StringBuilder hex = new StringBuilder(bytes.Length * 2);
+            foreach (byte b in bytes)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        public static string BytesToString(this byte[] bytes)
+        {
+            using (MemoryStream stream = new MemoryStream(bytes))
+            {
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+
+        public static byte[] HexadecimalStringToByteArray(this string input)
+        {
+            var outputLength = input.Length / 2;
+            var output = new byte[outputLength];
+            using (var sr = new StringReader(input))
+            {
+                for (var i = 0; i < outputLength; i++)
+                    output[i] = Convert.ToByte(new string(new char[2] { (char)sr.Read(),
+                    (char)sr.Read() }), 16);
+            }
+            return output;
+        }
+    }
+}
