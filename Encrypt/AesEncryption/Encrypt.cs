2a56bc56d94b5712e0cdf6d8aedff2200c939d33
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-04-09 09:42:51 +0100

    Add null check method

--- Encrypt/AesEncryption/Encrypt.cs
+++ Encrypt/AesEncryption/Encrypt.cs
@@ -1,27 +1,33 @@
-﻿using System.IO;
+﻿using System;
+using System.Text;
 using System.Security.Cryptography;
 
 namespace SafeCrypt
 {
     public class Encrypt : BaseAesEncryption
     {
-        public byte[] AesEncrypt(byte[] data, byte[] key, byte[] iv)
-        {
-            return EncryptAES(data, key, iv);
-        }
+        public byte[] AesEncrypt(byte[] data, byte[] secretKey, byte[] iv)
+            => EncryptAES(data, secretKey, iv);
+               
 
-        public string AesEncryptByteToHex(byte[] data, byte[] key, byte[] iv)
+        public string AesEncryptByteToHexString(byte[] data, byte[] secretKey, byte[] iv)
         {
-            var cipherText = EncryptAES(data, key, iv);
+            var cipherText = EncryptAES(data, secretKey, iv);
 
             return cipherText.ByteArrayToHexString();
         }
+        
 
-        public string AesEncryptByteToBase64(byte[] data, byte[] key, byte[] iv)
+        private void NullChecks(string data, string secretKey, string iv)
         {
-            var cipherText = EncryptAES(data, key, iv);
+            if (data == null || data.Length <= 0)
+                throw new ArgumentNullException("plainText");
 
-            return cipherText.ByteArrayToHexString();
+            if (secretKey == null || secretKey.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException("IV");
         }
 
         //public byte[] AesEncrypt(byte[] data, byte[] key, byte[] iv, ReturnType returnType)
