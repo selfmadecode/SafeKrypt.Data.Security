be0d6a13d3eeae2841bc886eabd0cd3374476702
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-12-15 16:22:46 +0100

    feat: add method convert string  to Hex string

--- src/Helpers/Converters.cs
+++ src/Helpers/Converters.cs
@@ -37,10 +37,28 @@ namespace SafeCrypt.src.Helpers
             }
             return output;
         }
+        /// <summary>
+        /// Converts a string to its hexadecimal representation.
+        /// </summary>
+        /// <param name="input">The input string to convert.</param>
+        /// <returns>The hexadecimal representation of the input string.</returns>
+
+        public static string ConvertToHexString(this string input)
+        {
+            byte[] byteArray = Encoding.UTF8.GetBytes(input);
+            StringBuilder hexBuilder = new StringBuilder(byteArray.Length * 2);
+
+            foreach (byte b in byteArray)
+            {
+                hexBuilder.AppendFormat("{0:X2}", b);
+            }
+
+            return hexBuilder.ToString();
+        }
 
         public static byte[] ConvertKeysToBytes(this string data)
         {
             return Encoding.UTF8.GetBytes(data);
-        }
+        }        
     }
 }
