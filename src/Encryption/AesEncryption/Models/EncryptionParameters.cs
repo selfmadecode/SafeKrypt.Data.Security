420e464e81abaaa36328f5cee70b4ce19f7caab8
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-12-15 14:17:21 +0100

    feat: add string encryption paramter class to encapsulate the iv, secret and data

--- src/Encryption/AesEncryption/Models/EncryptionParameters.cs
+++ src/Encryption/AesEncryption/Models/EncryptionParameters.cs
@@ -30,6 +30,25 @@ namespace SafeCrypt.src.Encryption.AesEncryption.Models
     }
         
 
-    
+    public class StringEncryptionParameters
+    {
+        /// <summary>
+        /// Gets or sets the data to be encrypted.
+        /// </summary>
+        [Required]
+        public string Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets the secret key used for encryption.
+        /// </summary>
+        [Required]
+        public string SecretKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the initialization vector (IV) used for encryption.
+        /// </summary>
+        [Required]
+        public string IV { get; set; }
+    }
 
 }
