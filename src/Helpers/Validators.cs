eeb0ad72d8beaf341b20e448db02296f188af351
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-12-15 16:23:20 +0100

    feat: add null checks for EncryptionParameters

--- src/Helpers/Validators.cs
+++ src/Helpers/Validators.cs
@@ -15,16 +15,16 @@ namespace SafeCrypt.src.Helpers
         /// <exception cref="ArgumentNullException">
         /// Thrown if the specified parameters instance is null or if any of its required properties are null.
         /// </exception>
-        public static void ValidateNotNull(ByteEncryptionParameters parameters)
+        public static void ValidateNotNull(EncryptionParameters parameters)
         {
             if (parameters == null)
             {
                 throw new ArgumentNullException(nameof(parameters), "ByteEncryptionParameters instance cannot be null.");
             }
 
-            if (parameters.Data == null)
+            if (parameters.DataToEncrypt == null)
             {
-                throw new ArgumentNullException(nameof(parameters.Data), "Data property cannot be null.");
+                throw new ArgumentNullException(nameof(parameters.DataToEncrypt), "DataToEncrypt property cannot be null.");
             }
 
             if (parameters.SecretKey == null)
@@ -58,6 +58,29 @@ namespace SafeCrypt.src.Helpers
             if (parameters.IV == null)
             {
                 throw new ArgumentNullException(nameof(parameters.IV), "IV property cannot be null.");
+            }
+        }
+
+        /// <summary>
+        /// Validates whether the provided string is a valid Base64-encoded key.
+        /// </summary>
+        /// <param name="keyAsString">The string to validate.</param>
+        /// <returns>True if the string is a valid Base64-encoded key; otherwise, false.</returns>
+        public static bool IsBase64String(string keyAsString)
+        {
+            if (string.IsNullOrEmpty(keyAsString))
+            {
+                return false;
+            }
+
+            try
+            {
+                byte[] data = Convert.FromBase64String(keyAsString);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
             }
         }
     }
