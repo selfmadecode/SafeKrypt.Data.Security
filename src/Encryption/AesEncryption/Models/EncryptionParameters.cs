c378553d59ce5f7d53264868683bb3c7c5e70249
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-12-15 18:09:54 +0100

    feat: set encryption property to string

--- src/Encryption/AesEncryption/Models/EncryptionParameters.cs
+++ src/Encryption/AesEncryption/Models/EncryptionParameters.cs
@@ -1,31 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace SafeCrypt.src.Encryption.AesEncryption.Models
 {
-    /// <summary>
-    /// Represents the parameters required for encryption.
-    /// </summary>
-    public class ByteEncryptionParameters
+    public class EncryptionParameters
     {
         /// <summary>
         /// Gets or sets the data to be encrypted.
         /// </summary>
         [Required]
-        public byte[] Data { get; set; }
+        public string DataToEncrypt { get; set; }
 
         /// <summary>
         /// Gets or sets the secret key used for encryption.
         /// </summary>
         [Required]
-        public byte[] SecretKey { get; set; }
+        public string SecretKey { get; set; }
 
         /// <summary>
         /// Gets or sets the initialization vector (IV) used for encryption.
         /// </summary>
         [Required]
-        public byte[] IV { get; set; }
+        public string IV { get; set; }
     }
-        
 
     public class StringEncryptionParameters
     {
