40615ef204f29b1ea02d23fb48d4f581b47fc69d
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-12-15 18:03:06 +0100

    feat: add error property and list of errors to EncryptionData

--- src/Encryption/AesEncryption/Models/EncryptionData.cs
+++ src/Encryption/AesEncryption/Models/EncryptionData.cs
@@ -12,11 +12,19 @@ namespace SafeCrypt.src.Encryption.AesEncryption.Models
         /// <summary>
         /// Gets or sets the data to be encrypted.
         /// </summary>
-        public byte[] Data { get; set; }
+        public string EncryptedData { get; set; }
 
         /// <summary>
         /// Gets or sets the initialization vector (IV) used for encryption.
         /// </summary>
-        public byte[] Iv { get; set; }
+        public string Iv { get; set; }
+
+        /// <summary>
+        /// Gets or sets the secret key used for encryption. Should be a base64 string
+        /// </summary>
+        public string SecretKey { get; set; }
+
+        public bool HasError { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
     }
 }
