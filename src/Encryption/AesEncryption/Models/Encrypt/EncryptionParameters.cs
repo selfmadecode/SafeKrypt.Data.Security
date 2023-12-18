1d3e017a32b66435660ddb12d2902fe1135b9a80
eanolue@sbsc.com <eanolue@sbsc.com>
2023-12-18 04:47:52 +0100

    removed unnecessary using statements

--- src/Encryption/AesEncryption/Models/Encrypt/EncryptionParameters.cs
+++ src/Encryption/AesEncryption/Models/Encrypt/EncryptionParameters.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace SafeCrypt.Models
 {
--- src/Helpers/Validators.cs
+++ src/Helpers/Validators.cs
@@ -1,7 +1,5 @@
 ﻿using SafeCrypt.Models;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SafeCrypt.Helpers
 {
