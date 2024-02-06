4b028498b34be04677a5cb6092ae5c5286470933
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-02-06 14:44:57 +0100

    feat: add RsaEncryptionParameters to hold parameters for encryption

---
+++ src/SafeCrypt.Lib/Encryption/RsaEncryption/Models/Encrypt/RsaEncryptionParameters.cs
@@ -0,0 +1,20 @@
+﻿namespace SafeCrypt.RsaEncryption.Models
+{
+    internal class RsaEncryptionParameters : IEncryptionData
+    {
+        /// <summary>
+        /// Gets or sets the public key for RSA encryption.
+        /// </summary>
+        public string PublicKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the private key for RSA encryption.
+        /// </summary>
+        public string PrivateKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the data to be encrypted using RSA.
+        /// </summary>
+        public string DataToEncrypt { get; set; }
+    }
+}
