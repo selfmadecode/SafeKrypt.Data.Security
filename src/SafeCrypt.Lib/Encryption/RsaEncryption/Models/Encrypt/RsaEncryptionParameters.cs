c50d51e85131ddaaa6817b041ff2041209d570f1
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-02-07 14:09:07 +0100

    feat: use byte[] to hold encrypted data

--- src/SafeCrypt.Lib/Encryption/RsaEncryption/Models/Encrypt/RsaEncryptionParameters.cs
+++ src/SafeCrypt.Lib/Encryption/RsaEncryption/Models/Encrypt/RsaEncryptionParameters.cs
@@ -1,20 +1,34 @@
-﻿namespace SafeCrypt.RsaEncryption.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SafeCrypt.RsaEncryption.Models
 {
-    internal class RsaEncryptionParameters : IEncryptionData
+    public sealed class RsaEncryptionParameters : IEncryptionData
     {
         /// <summary>
         /// Gets or sets the public key for RSA encryption.
         /// </summary>
-        public string PublicKey { get; set; }
+        [Required]
+        public string PublicKey { get; set; }       
 
         /// <summary>
-        /// Gets or sets the private key for RSA encryption.
+        /// Gets or sets the data to be encrypted using RSA.
         /// </summary>
+        [Required]
+        public string DataToEncrypt { get; set; }
+    }
+
+    public sealed class RsaDecryptionParameters 
+    {
+        /// <summary>
+        /// Gets or sets the public key for RSA encryption.
+        /// </summary>
+        [Required]
         public string PrivateKey { get; set; }
 
         /// <summary>
         /// Gets or sets the data to be encrypted using RSA.
         /// </summary>
-        public string DataToEncrypt { get; set; }
+        [Required]
+        public byte[] DataToDecrypt { get; set; }
     }
 }
