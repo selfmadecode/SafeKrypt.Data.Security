caecaf7681411de49d755e1a96946dad3d041a60
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-02-26 00:03:52 +0100

    cleanup encryption parameter models

--- src/SafeCrypt.Lib/Encryption/AesEncryption/Models/Decrypt/DecryptionParameters.cs
+++ src/SafeCrypt.Lib/Encryption/AesEncryption/Models/Decrypt/DecryptionParameters.cs
@@ -1,28 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-
-namespace SafeCrypt.Models
+﻿namespace SafeCrypt.Models
 {
-    public class DecryptionParameters
+    public class DecryptionParameters : BaseAesEncryptionParameters
     {
-        /// <summary>
-        /// Gets or sets the data to be decrypted.
-        /// </summary>
-        [Required]
-        public string DataToDecrypt { get; set; }
-
-        /// <summary>
-        /// Gets or sets the secret key used for decryption.
-        /// </summary>
-        [Required]
-        public string SecretKey { get; set; }
-
-        /// <summary>
-        /// Gets or sets the initialization vector (IV) used for decryption.
-        /// </summary>
-        [Required]
-        public string IV { get; set; }
     }
 }
--- src/SafeCrypt.Lib/Encryption/AesEncryption/Models/Encrypt/EncryptionParameters.cs
+++ src/SafeCrypt.Lib/Encryption/AesEncryption/Models/Encrypt/EncryptionParameters.cs
@@ -1,48 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-
-namespace SafeCrypt.Models
+﻿namespace SafeCrypt.Models
 {
-    public class EncryptionParameters
+    public class EncryptionParameters : BaseAesEncryptionParameters
     {
-        /// <summary>
-        /// Gets or sets the data to be encrypted.
-        /// </summary>
-        [Required]
-        public string DataToEncrypt { get; set; }
-
-        /// <summary>
-        /// Gets or sets the secret key used for encryption.
-        /// </summary>
-        [Required]
-        public string SecretKey { get; set; }
-
-        /// <summary>
-        /// Gets or sets the initialization vector (IV) used for encryption.
-        /// </summary>
-        [Required]
-        public string IV { get; set; }
     }
 
-    public class StringEncryptionParameters
+    public class StringEncryptionParameters : BaseAesEncryptionParameters
     {
-        /// <summary>
-        /// Gets or sets the data to be encrypted.
-        /// </summary>
-        [Required]
-        public string Data { get; set; }
-
-        /// <summary>
-        /// Gets or sets the secret key used for encryption.
-        /// </summary>
-        [Required]
-        public string SecretKey { get; set; }
-
-        /// <summary>
-        /// Gets or sets the initialization vector (IV) used for encryption.
-        /// </summary>
-        [Required]
-        public string IV { get; set; }
     }
-
 }
