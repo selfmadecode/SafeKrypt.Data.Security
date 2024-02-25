c3af5fc1b9ab3fa92441b5f71d37462eb07b755c
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-02-25 23:53:49 +0100

    add BaseAesEncryptionParameters

---
+++ src/SafeCrypt.Lib/Encryption/AesEncryption/Models/BaseAesEncryptionParameters.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SafeCrypt.Models
+{
+    public class BaseAesEncryptionParameters
+    {
+        /// <summary>
+        /// Gets or sets the data to be encrypted.
+        /// </summary>
+        [Required]
+        public string Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets the secret key used for encryption.
+        /// </summary>
+        [Required]
+        public string SecretKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the initialization vector (IV) used for encryption.
+        /// </summary>
+        [Required]
+        public string IV { get; set; }
+    }
+}
