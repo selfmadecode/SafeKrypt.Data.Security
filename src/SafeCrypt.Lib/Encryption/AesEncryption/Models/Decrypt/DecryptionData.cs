d8c675c5195fd963085c004526c6ace3ed2b5220
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-02-27 13:22:55 +0100

    update decryption data to use BaseAesData

--- src/SafeCrypt.Lib/Encryption/AesEncryption/Models/Decrypt/DecryptionData.cs
+++ src/SafeCrypt.Lib/Encryption/AesEncryption/Models/Decrypt/DecryptionData.cs
@@ -2,24 +2,11 @@
 
 namespace SafeCrypt.Models
 {
-    public class DecryptionData
+    public class DecryptionData : BaseAesData
     {
         /// <summary>
-        /// Gets or sets the data to be encrypted.
+        /// Gets or sets the data to be decrypted.
         /// </summary>
         public string DecryptedData { get; set; }
-
-        /// <summary>
-        /// Gets or sets the initialization vector (IV) used for encryption.
-        /// </summary>
-        public string Iv { get; set; }
-
-        /// <summary>
-        /// Gets or sets the secret key used for encryption. Should be a base64 string
-        /// </summary>
-        public string SecretKey { get; set; }
-
-        public bool HasError { get; set; }
-        public List<string> Errors { get; set; } = new List<string>();
     }
 }
