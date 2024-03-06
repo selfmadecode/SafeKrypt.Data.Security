83842a6b8bce30decb863d4d36b84a95cb13821c
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2024-03-06 11:39:23 +0100

    add test for Hex encrypt and decrypt methods

---
+++ src/SafeCrypt.UnitTests/AesTests/Encryption.cs
@@ -0,0 +1,44 @@
+﻿using SafeCrypt.Helpers;
+using SafeCrypt.Models;
+using SafeCrypt.AES;
+
+namespace SafeCrypt.UnitTests.AesTests;
+
+public class Encryption
+{
+    public const string Data = "Hello, World!";
+
+    [Fact]
+    public async Task EncryptToHexStringAsync_And_DecryptFromHexStringAsync_ValidParameters_ReturnsOriginalData()
+    {
+        // Arrange
+        var aesIv = KeyGenerators.GenerateHexadecimalIVKey();
+        var secret = KeyGenerators.GenerateAesSecretKey(256);
+
+        var encryptionParameters = new EncryptionParameters
+        {
+            Data = Data,
+            IV = aesIv,
+            SecretKey = secret
+        };
+
+        // Act
+        var encryptedResult = await Aes.EncryptToHexStringAsync(encryptionParameters);
+        var decryptedResult = await Aes.DecryptFromHexStringAsync(new DecryptionParameters
+        {
+            Data = encryptedResult.EncryptedData,
+            IV = aesIv,
+            SecretKey = secret
+        });
+
+        // Assert
+        Assert.NotNull(encryptedResult.EncryptedData);
+        Assert.Equal(Data, decryptedResult.DecryptedData);
+
+        Assert.False(encryptedResult.HasError);
+        Assert.False(decryptedResult.HasError);
+
+        Assert.Empty(encryptedResult.Errors);
+        Assert.Empty(decryptedResult.Errors);
+    }
+}
