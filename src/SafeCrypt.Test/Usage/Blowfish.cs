30edb2f0dda73375fe2e17fbb2214fd48830e0fc
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-02-11 18:30:20 +0100

    test blowfis algorithm

---
+++ src/SafeCrypt.Test/Usage/Blowfish.cs
@@ -0,0 +1,27 @@
+﻿using SafeCrypt.Encryption;
+using SafeCrypt.Helpers;
+using SafeCrypt.Encryption.BlowfishEncryption;
+
+namespace safecrypt_testapp.Usage
+{
+    public static class BlowfishTest
+    {
+        public static void Test()
+        {
+            // Generate a random key (128 bits in this example)
+            int keySize = 128; // Todo: validate keysize, it can be 32, 64, ..., 448
+            byte[] key = KeyGenerators.GenerateBlowfishKey(keySize);
+
+            string plainText = "Hello, Blowfish!";
+            Console.WriteLine($"Original Text: {plainText}");
+
+            // Encrypt
+            string encryptedText = Blowfish.Encrypt(plainText, key);
+            Console.WriteLine($"Encrypted Text (Base64): {encryptedText}");
+
+            // Decrypt
+            string decryptedText = Blowfish.Decrypt(encryptedText, key);
+            Console.WriteLine($"Decrypted Text: {decryptedText}");
+        }
+    }
+}
